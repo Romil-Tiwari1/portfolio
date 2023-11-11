1baf780b0c7ddf23a6f46498035d59e7b9a2d132
Romil Tiwari <tiwari.rom@northeastern.edu>
2023-11-10 23:07:33 -0500

    Small fixes

--- update_pages_domain.sh
+++ update_pages_domain.sh
@@ -4,11 +4,13 @@
 source .env
 
 # Variables
+USERNAME="Romil-Tiwari1"
 REPO="Portfolio"
 DOMAIN="romiltiwari.com"
+TOKEN="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"
 
 # GitHub API URL for Pages
-API_URL="https://api.github.com/repos/$GITHUB_USERNAME/$REPO/pages"
+API_URL="https://api.github.com/repos/$USERNAME/$REPO/pages"
 
 # JSON Payload
 JSON_DATA=$(cat <<EOF
@@ -21,7 +23,7 @@ EOF
 
 # Call GitHub API to update Pages configuration
 curl -X PATCH \
-     -H "Authorization: token $REACT_APP_GITHUB_TOKEN" \
+     -H "Authorization: token $TOKEN" \
      -H "Accept: application/vnd.github.v3+json" \
      -d "$JSON_DATA" \
      $API_URL
